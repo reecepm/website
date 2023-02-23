60bcdd796d01f86901546d34db51cb2508e0addc
Reece <cascoidmc@gmail.com>
2023-02-23 20:14:42 +0000

    fix tag

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -9,16 +9,12 @@ const Tag: React.FC<Props> = ({ children, background }) => {
   return (
     <div
       className="flex items-center justify-center rounded-full border border-amber-700/20 bg-amber-900/40 py-1 px-2.5 text-center text-xs text-amber-600 transition-all md:py-1.5 md:px-3 md:font-medium"
-      style={
-        background
-          ? {
-              borderColor: `rgba(${background || "120, 53, 15"}, 0.2)`,
-              color: `rgb(${background || "180 83 9"})`,
-              backgroundColor: `rgba(${background || "120, 53, 15"}, 0.2)`,
-              filter: "saturate(1.5)",
-            }
-          : undefined
-      }
+      style={{
+        borderColor: `rgba(${background || "120, 53, 15"}, 0.2)`,
+        color: `rgb(${background || "180 83 9"})`,
+        backgroundColor: `rgba(${background || "120, 53, 15"}, 0.2)`,
+        filter: "saturate(1.5)",
+      }}
     >
       {children}
     </div>
