ee7d6f5b019f47a9afb11dad443328a6aa622c38
Reece <cascoidmc@gmail.com>
2023-02-23 03:17:59 +0000

    idk

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { domAnimation, LazyMotion } from "framer-motion";
+import { domMax, LazyMotion } from "framer-motion";
 import React from "react";
 import CarouselBase from "../../components/work/carousel/Base";
 
 const Work: React.FC = () => {
   return (
-    <LazyMotion features={domAnimation}>
+    <LazyMotion features={domMax}>
       <CarouselBase />
     </LazyMotion>
   );
